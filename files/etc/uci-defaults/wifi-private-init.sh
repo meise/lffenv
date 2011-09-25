352539c4802ac3deda868dfb5372a44d1c0ebc53
Linus Lüssing <linus.luessing@web.de>
2011-09-25 17:52:03 +0200

    wireless: Adding private WPA secured Wifi interface
    This commit adds another wifi interface in AP mode, bridged with the WAN
    port / private network. This allows the user to also use the same device
    for own, local network usage, with no need to have another, separate
    device for the home network.
    
    This WiFi interface is secured with WPA and a random 63 character
    passphrase which can be looked up or changed in the LuCI web interface
    (or command line, of course).
    
    Signed-off-by: Linus Lüssing <linus.luessing@web.de>

---
+++ files/etc/uci-defaults/wifi-private-init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+KEY="`pwgen -c -n -s 63 1`"
+MAC="`ip link show eth0 | grep "link/ether" | \
+	sed "s/^[ ]*//" | cut -d' ' -f2 | sed "s/://g"`"
+
+uci set wireless.wifi_private.key="$KEY"
+uci set wireless.wifi_private.ssid="private-$MAC"
+uci commit
