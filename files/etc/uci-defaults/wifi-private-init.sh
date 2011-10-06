a92f3fb422e8982c2a9ebbaedd169ebc9e84bf0a
Linus Lüssing <linus.luessing@web.de>
2011-10-06 16:52:43 +0200

    wireless: Use capital MAC addresses
    ...as also below the devices the MAC is usually written with capital
    letters - just to avoid confusion.
    
    Signed-off-by: Linus Lüssing <linus.luessing@web.de>

--- files/etc/uci-defaults/wifi-private-init.sh
+++ files/etc/uci-defaults/wifi-private-init.sh
@@ -2,7 +2,8 @@
 
 KEY="`pwgen -c -n -s 63 1`"
 MAC="`ip link show eth0 | grep "link/ether" | \
-	sed "s/^[ ]*//" | cut -d' ' -f2 | sed "s/://g"`"
+	sed "s/^[ ]*//" | cut -d' ' -f2 | sed "s/://g" | \
+	tr 'a-z' 'A-Z'`"
 
 uci set wireless.wifi_private.key="$KEY"
 uci set wireless.wifi_private.ssid="private-$MAC"
