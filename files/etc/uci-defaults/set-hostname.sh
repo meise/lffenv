9bed33977b3acf0895f7254a8ae47ab9d214b209
Linus Lüssing <linus.luessing@web.de>
2011-10-10 03:53:42 +0200

    network: Fix hostname for some targets
    E.g. the MAC printed on the back of the Dlink DIR-300 is not exactly the
    one of eth0. It is actually the one of ath0/wifi0, but we do not have
    that information available yet during uci-defaults. Therefore we are
    adding a customization of the MAC for this profile. There might be more
    profiles later where the MAC is not exactly the one printed on the
    router, so using the new /etc/lff_profile seems to make sense to stay
    flexible.
    
    Signed-off-by: Linus Lüssing <linus.luessing@web.de>

--- files/etc/uci-defaults/set-hostname.sh
+++ files/etc/uci-defaults/set-hostname.sh
@@ -4,6 +4,11 @@ MAC="`ip link show eth0 | grep "link/ether" | \
 	sed "s/^[ ]*//" | cut -d' ' -f2 | sed "s/://g" | \
 	tr 'a-z' 'A-Z'`"
 
+# The MAC printed on the back of the router of the Dlink DIR-300
+# is one smaller than on eth0
+[ "`cat /etc/lff_profile`" = "lff-dlink-dir300" ] && \
+	MAC="`printf "%012X\n" $((0x$MAC - 0x01))`"
+
 uci set network.private.hostname="Freifunk-$MAC"
 uci set network.mesh.hostname="Freifunk-$MAC"
 uci set system.@system[0].hostname="Freifunk-$MAC"
