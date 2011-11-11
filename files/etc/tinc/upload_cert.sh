a94bee5acbfda29d83ea27de8e0a6ffd9b4f398d
yanosz <yanosz@gmx.net>
2011-11-11 22:42:30 +0100

    Fixes

---
+++ files/etc/tinc/upload_cert.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+UPLOAD_URL="https://apps.jluehr.de/ff-serv/tincs.txt"
+CURLC=`which curl`
+TINC_NAME="`ip link show eth0 | grep "link/ether" | sed "s/^[ ]*//" | cut -d' ' -f2 | sed "s/://g"`"
+CERT_FILE="/etc/tinc/intracity/hosts/$TINC_NAME"
+
+
+for cnt in $(seq 1 100);
+do
+        $CURLC -k -F "cert=@$CERT_FILE" $UPLOAD_URL -u $TINC_NAME:$TINC_NAME && break
+        sleep 10
+done
